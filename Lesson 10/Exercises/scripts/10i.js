a65ad7d21fdbae9201a00e97e216475cbada8ac2
Marek Pelowski <marek.pelowski@protonmail.com>
2023-10-18 13:08:36 +0200

    add the rest of the buttons

---
+++ Lesson 10/Exercises/scripts/10i.js
@@ -0,0 +1,21 @@
+let calculation = JSON.parse(localStorage.getItem('calculation')) || '';
+
+showCalculation();
+
+function updateCalculation(number) {
+  calculation += number;
+  showCalculation();
+  localStorage.setItem('calculation', JSON.stringify(calculation))
+}
+
+function showCalculation() {
+  document.querySelector('.js-calculation')
+    .innerHTML = calculation;
+}
+
+function clearCalculation() {
+  calculation = ''
+
+  document.querySelector('.js-calculation')
+  .innerHTML = '0';
+}
