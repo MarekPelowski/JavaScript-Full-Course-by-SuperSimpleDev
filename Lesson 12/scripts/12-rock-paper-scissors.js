b56e33e164ebaa955d6fa417c7dfcc633f8330c8
Marek Pelowski <marek.pelowski@protonmail.com>
2023-10-22 16:46:40 +0200

    add keydown

--- Lesson 12/scripts/12-rock-paper-scissors.js
+++ Lesson 12/scripts/12-rock-paper-scissors.js
@@ -65,6 +65,16 @@ document.querySelector('.js-reset-score-button')
     updateScoreElement();
   });
 
+document.body.addEventListener('keydown', (event) => {
+  if (event.key === 'r') {
+    playGame('rock');
+  } else if (event.key === 'p') {
+    playGame('paper');
+  } else if (event.key === 's') {
+    playGame('scissors');
+  }
+});
+
 
 
 function playGame(playerMove) {
