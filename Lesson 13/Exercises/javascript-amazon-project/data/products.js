d1384900636306f56afad6b08235b66456a20579
Marek Pelowski <marek.pelowski@protonmail.com>
2023-10-26 20:56:08 +0200

    Add two new products

--- Lesson 13/Exercises/javascript-amazon-project/data/products.js
+++ Lesson 13/Exercises/javascript-amazon-project/data/products.js
@@ -1,5 +1,25 @@
 const products = [
   {
+    id: 'id1',
+    image: "images/products/backpack.jpg",
+    name: "Black School Backpack",
+    rating: {
+      stars: 4,
+      count: 104
+    },
+    priceCents: 3099
+  },
+  {
+    id: 'id2',
+    image: "images/products/umbrella.jpg",
+    name: "Green anti-rain umbrella",
+    rating: {
+      stars: 5,
+      count: 1173
+    },
+    priceCents: 1599
+  },
+  {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     image: "images/products/athletic-cotton-socks-6-pairs.jpg",
     name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
