8688d3926d4be061995b63582bf0de30e198f972
Marek Pelowski <marek.pelowski@protonmail.com>
2023-10-26 20:23:25 +0200

    Make the cart quantity interactive

--- Lesson 13/javascript-amazon-project/scripts/amazon.js
+++ Lesson 13/javascript-amazon-project/scripts/amazon.js
@@ -84,7 +84,7 @@ document.querySelectorAll('.js-add-to-cart')
         cartQuantity += item.quantity;
       });
 
-      console.log(cartQuantity);
-      console.log(cart);
+      document.querySelector('.js-cart-quantity')
+        .innerHTML = cartQuantity
     });
   });
