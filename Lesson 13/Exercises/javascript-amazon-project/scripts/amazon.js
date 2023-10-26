593a52955e4e54b52095739ae22db606df775c12
Marek Pelowski <marek.pelowski@protonmail.com>
2023-10-26 20:48:53 +0200

    Add changing the quantity selector interactive

--- Lesson 13/Exercises/javascript-amazon-project/scripts/amazon.js
+++ Lesson 13/Exercises/javascript-amazon-project/scripts/amazon.js
@@ -25,7 +25,7 @@ products.forEach((product) => {
       </div>
 
       <div class="product-quantity-container">
-        <select>
+        <select class="js-quantity-selector-${product.id}">
           <option selected value="1">1</option>
           <option value="2">2</option>
           <option value="3">3</option>
@@ -60,6 +60,8 @@ document.querySelectorAll('.js-add-to-cart')
   .forEach((button) => {
     button.addEventListener('click', () => {
       const productId = button.dataset.productId;
+      const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
+
 
       let matchingItem;
 
@@ -70,11 +72,11 @@ document.querySelectorAll('.js-add-to-cart')
       });
 
       if (matchingItem) {
-        matchingItem.quantity += 1;
+        matchingItem.quantity += Number(quantitySelector.value);
       } else {
         cart.push({
           productId: productId,
-          quantity: 1
+          quantity: Number(quantitySelector.value)
         });
       }
 
@@ -86,5 +88,8 @@ document.querySelectorAll('.js-add-to-cart')
 
       document.querySelector('.js-cart-quantity')
         .innerHTML = cartQuantity
+
+      console.log(cartQuantity);
+      console.log(cart);
     });
   });
