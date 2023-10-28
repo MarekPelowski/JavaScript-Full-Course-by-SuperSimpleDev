312145027b0ea74c4aafa4cd002891fa482cb677
Marek Pelowski <marek.pelowski@protonmail.com>
2023-10-28 15:14:15 +0200

    Update the checkout text

--- Lesson 14/Exercises/javascript-amazon-project/scripts/checkout.js
+++ Lesson 14/Exercises/javascript-amazon-project/scripts/checkout.js
@@ -109,5 +109,19 @@ document.querySelectorAll('.js-delete-link')
         `.js-cart-item-container-${productId}`
       )
       container.remove();
+      updateCartQuantity();
     });
   });
+
+function updateCartQuantity() {
+  let cartQuantity = 0;
+  
+  cart.forEach((cartItem) => {
+    cartQuantity += cartItem.quantity;
+  });
+  
+  document.querySelector('.js-return-to-home-link')
+    .innerHTML = cartQuantity
+}
+
+updateCartQuantity();
