51f80ed044df559e58eb1ac5e26e890fe5728f56
Marek Pelowski <marek.pelowski@protonmail.com>
2023-10-28 19:45:47 +0200

    Create an input and a save link to update the quantity

--- Lesson 14/Exercises/javascript-amazon-project/scripts/checkout.js
+++ Lesson 14/Exercises/javascript-amazon-project/scripts/checkout.js
@@ -37,8 +37,13 @@ cart.forEach((cartItem) => {
             <span>
               Quantity: <span class="quantity-label">${cartItem.quantity}</span>
             </span>
-            <span class="update-quantity-link link-primary">
+            <span class="update-quantity-link link-primary js-update-link"
+            data-product-id=${matchingProduct.id}>
               Update
+            </span>
+            <input class="quantity-input">
+            <span class="save-quantity-link link-primary">
+              Save
             </span>
             <span class="delete-quantity-link link-primary js-delete-link"
             data-product-id=${matchingProduct.id}>
@@ -111,6 +116,14 @@ document.querySelectorAll('.js-delete-link')
       container.remove();
       updateCartQuantity();
     });
+  });
+
+document.querySelectorAll('.js-update-link')
+  .forEach((link) => {
+    link.addEventListener('click', () => {
+      const productId = link.dataset.productId;
+      console.log(productId);
+    })
   });
 
 function updateCartQuantity() {
